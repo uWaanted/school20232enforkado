527ef315ff0df3a8f7d2640d9e2703affd9b452d
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-11-07 23:18:34 +0000

    add: course

---
+++ app/admin/course/page.tsx
@@ -0,0 +1,17 @@
+export default function ListCourse(){
+    return (
+        <div>
+        <h1 className="text-center">Lista de Cursos</h1>
+
+        <table>
+            <thead>
+                <tr> <td> Linha1 Col1 </td><td> Linha1 Col2</td></tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+
+        </div>
+    )
+}
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,9 @@
 export default function Admin(){
     return (
+        <div>
         <h1 className="text-center text-xl"> O ADM ESTÁ ONLINE</h1>
+        <a href="/admin/course"> Listar Cursos </a>
+
+        </div>
     )
 }
