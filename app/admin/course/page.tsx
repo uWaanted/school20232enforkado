f7f395e0f2f56e78418cb2f18c256a57e0098bb0
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-11-07 23:34:23 +0000

    add: um monte de coisa

--- app/admin/course/page.tsx
+++ app/admin/course/page.tsx
@@ -1,14 +1,23 @@
-export default function ListCourse(){
+import { sql } from "@vercel/postgres"
+
+export default async function ListCourse(){
+    const { rows } = await sql`SELECT * FROM courses`; 
     return (
         <div>
         <h1 className="text-center">Lista de Cursos</h1>
 
         <table>
             <thead>
-                <tr> <td> Linha1 Col1 </td><td> Linha1 Col2</td></tr>
+                <tr> <td> Título do Curso </td><td> Descrição</td></tr>
             </thead>
             <tbody>
-                
+                {
+                rows.map((course) => {
+                    return (
+                    <tr key={course.id}><td>{course.title}</td> <td> {course.description}</td></tr>
+                    )
+                })
+                }
             </tbody>
         </table>
 
