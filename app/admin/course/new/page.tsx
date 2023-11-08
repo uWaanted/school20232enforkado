cbde018e1647fb3d4e9b11c3d1720ab9bba87d3d
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-11-08 01:13:40 +0000

    commit e push pra dar uma olhadinha

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -1,12 +1,16 @@
-export default function NewCourse(){
+export default async function NewCourse(){
+    function saveCourse(){
+        console.log("Acessou a função.")
+    }
+    
     return (
         <div>
-            <h1 className="text-white font-xl"> Cadastrar Cursos </h1>
+            <h1 className="text-white text-center text-4xl"> Cadastrar Cursos </h1>
             <form>
-                <input type="text" placeholder="Digite o Título do Curso"/><br/>
-                <input type="text" placeholder="Digite a Descrição do Curso"/><br/>
-                <input type="text" placeholder="Digite a URL da Imagem"/><br/>
-                <button className="text-white"> Salvar </button>
+                <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/>
+                <input type="text" name="description" placeholder="Digite a Descrição do Curso"/><br/>
+                <input type="text" name="url" placeholder="Digite a URL da Imagem"/><br/>
+                <button formAction={saveCourse} className="text-white"> Salvar </button>
             </form>
         </div>
     )
