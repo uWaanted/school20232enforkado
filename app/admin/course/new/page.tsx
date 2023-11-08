605802b4fad428b4eeb7888acc549d9ca07b5cf2
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-11-08 01:46:05 +0000

    commit insert

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -1,8 +1,19 @@
-export default async function NewCourse(){
-    function saveCourse(){
-        console.log("Acessou a função.")
-    }
+import { sql } from "@vercel/postgres";
+
+export const revalidate =0
+
+export default function NewCourse(){
+    async function saveCourse(formData: FormData){
+        "use server"
+        const title = formData.get("title") as string;
+        const description = formData.get("description") as string;
+        const url = formData.get("url") as string;
+        console.log(title,description,url)
     
+         await sql `INSERT INTO courses(title, description, url) VALUES ( ${title}, ${description}, ${url} )`;
+        
+    }
+
     return (
         <div>
             <h1 className="text-white text-center text-4xl"> Cadastrar Cursos </h1>
