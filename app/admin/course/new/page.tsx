1c63fb54df1c07535e29182baeb6a34cc9b79d23
Henrique <62362076+uWaanted@users.noreply.github.com>
2023-11-22 00:39:28 +0000

    mande noticias do mundo de la

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -1,30 +1,37 @@
 import UploadButton from "@/app/components/UploadButton";
 import { sql } from "@vercel/postgres";
+import { useSearchParams } from "next/navigation";
 
 export const revalidate =0
 
-export default function NewCourse(){
+export default function NewCourse({
+    searchParams,
+  }: {
+    searchParams?: {
+      url?: string;  
+    };
+  }){
+    
+    const urlImage = searchParams?.url || '';
+
     async function saveCourse(formData: FormData){
         "use server"
         const title = formData.get("title") as string;
         const description = formData.get("description") as string;
-        const url = formData.get("url") as string;
-        console.log(title,description,url)
-    
-         await sql `INSERT INTO courses(title, description, url) VALUES ( ${title}, ${description}, ${url} )`;
-        
+        await sql`INSERT INTO courses (title, description,url) VALUES(${title}, ${description}, ${urlImage})`
+        console.log("Acessou a função")
     }
-
     return (
         <div>
-            <h1 className="text-white text-center text-4xl"> Cadastrar Cursos </h1>
+            <h1 className="text-white text-center text-4xl">Cadastrar Cursos</h1>
             <form>
-                <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/>
-                <input type="text" name="description" placeholder="Digite a Descrição do Curso"/><br/>
-                <input type="text" name="url" placeholder="Digite a URL da Imagem"/><br/>
+                <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/><br/>
+                <input type="text" name="description" placeholder="Digite a Descriçao do curso"/> <br/><br/>
+                <br/>
                 <UploadButton />
-                <button formAction={saveCourse} className="text-white"> Salvar </button>
+                <button formAction={saveCourse} className="text-white">Salvar</button>
             </form>
         </div>
+
     )
 }
--- app/components/UploadButton.tsx
+++ app/components/UploadButton.tsx
@@ -1,16 +1,20 @@
 "use client"
-
 import { CldUploadButton } from "next-cloudinary";
+import {  useRouter } from "next/navigation";
 
 export default function UploadButton(){
+    const router = useRouter()
+    
     function handleUpload(result:any, widget:any){
+
+       router.push(`?url=${result?.info?.url}`)
 
         widget.close()
     }
     return(
         <CldUploadButton
         onUpload={handleUpload}
-        uploadPreset="id"
+        uploadPreset="zfyjlzdd"
         />
     )
 }
